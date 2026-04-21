3cc1ecf9b9ab996c5cf7de467f014bdcb4daae01
Richard Davisson <71906941+Chessgeek-svg@users.noreply.github.com>
2026-04-20 17:56:28 -0700

    Add: edit-distance - submission-3

---
+++ Data Structures & Algorithms/edit-distance/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        ROWS, COLS = len(word1), len(word2)
+        dp = [[0 for col in range(COLS+1)] for row in range(ROWS+1)]
+
+        for i in range(ROWS):
+            dp[i][-1] = abs(ROWS - i)
+        for j in range(COLS):
+            dp[-1][j] = abs(COLS - j)
+
+        for i in range(ROWS -1, -1, -1):
+            for j in range(COLS -1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                else:
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j+1], dp[i+1][j+1])
+
+        print(dp)
+        
+        return dp[0][0]
