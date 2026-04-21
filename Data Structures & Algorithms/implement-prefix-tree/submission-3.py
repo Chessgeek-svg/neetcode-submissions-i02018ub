fd8b18730fbae8467ac12d97b7b4a0cd98817811
Richard Davisson <71906941+Chessgeek-svg@users.noreply.github.com>
2026-04-21 10:39:21 -0700

    Add: implement-prefix-tree - submission-3

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-3.py
@@ -0,0 +1,38 @@
+class TreeNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.word = False
+class PrefixTree:
+    def __init__(self):
+        self.root = TreeNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            char = ord(char) - ord('a')
+            if not curr.children[char]:
+                curr.children[char] = TreeNode()
+            curr = curr.children[char]
+        curr.word = True
+        return None
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for char in word:
+            char = ord(char) - ord('a')
+            if not curr.children[char]:
+                return False
+            curr = curr.children[char]
+        return curr.word
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for char in prefix:
+            char = ord(char) - ord('a')
+            if not curr.children[char]:
+                return False
+            curr = curr.children[char]
+        return True
+        
+        
